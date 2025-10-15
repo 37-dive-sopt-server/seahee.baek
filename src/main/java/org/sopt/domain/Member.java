720c36e47ee0610389a90001769e9f9831c6dc02
seahee <lollol0617@gmail.com>
2025-10-15 23:24:58 +0900

    [#1] feat: Member 클래스에 변수 추가, gender enum 생성

--- src/main/java/org/sopt/domain/Member.java
+++ src/main/java/org/sopt/domain/Member.java
@@ -1,13 +1,23 @@
 package org.sopt.domain;
 
+import java.time.LocalDate;
+
+import org.sopt.domain.enums.GENDER;
+
 public class Member {
 
-	private Long id;
-	private String name;
+	private final Long id;
+	private final String name;
+	private final LocalDate birthday;
+	private final String email;
+	private final GENDER gender;
 
-	public Member(Long id, String name) {
+	public Member(Long id, String name, LocalDate birthday, String email, GENDER gender) {
 		this.id = id;
 		this.name = name;
+		this.birthday = birthday;
+		this.email = email;
+		this.gender = gender;
 	}
 
 	public Long getId() {
@@ -16,5 +26,17 @@ public class Member {
 
 	public String getName() {
 		return name;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public GENDER getGender() {
+		return gender;
 	}
 }
