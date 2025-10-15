8b80ebd30756802b2718c0f009c56dd8a768965e
seahee <lollol0617@gmail.com>
2025-10-15 23:25:57 +0900

    [#1] feat: 이메일 중복검사 로직 작성

--- src/main/java/org/sopt/repository/MemoryMemberRepository.java
+++ src/main/java/org/sopt/repository/MemoryMemberRepository.java
@@ -10,9 +10,7 @@ import org.sopt.domain.Member;
 
 public class MemoryMemberRepository {
 
-
 	private static final Map<Long, Member> store = new HashMap<>();
-
 
 	public Member save(Member member) {
 
@@ -21,13 +19,18 @@ public class MemoryMemberRepository {
 
 	}
 
-
 	public Optional<Member> findById(Long id) {
 		return Optional.ofNullable(store.get(id));
 	}
 
-
 	public List<Member> findAll() {
 		return new ArrayList<>(store.values());
+	}
+
+	public Optional<Member> findByEmail(String email) {
+		return store.values()
+			.stream()
+			.filter(member -> member.getEmail().equals(email))
+			.findFirst();
 	}
 }
--- src/main/java/org/sopt/service/MemberServiceImpl.java
+++ src/main/java/org/sopt/service/MemberServiceImpl.java
@@ -1,9 +1,13 @@
 package org.sopt.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 import org.sopt.domain.Member;
+import org.sopt.domain.enums.GENDER;
+import org.sopt.exception.DuplicatedEmailException;
+import org.sopt.exception.util.ErrorMessage;
 import org.sopt.repository.MemoryMemberRepository;
 
 public class MemberServiceImpl implements MemberService {
@@ -11,9 +15,17 @@ public class MemberServiceImpl implements MemberService {
 	private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 	private static long sequence = 1L;
 
-	public Long join(String name) {
+	public Long join(String name, LocalDate birthday, String email, GENDER gender) {
+		System.out.println("이메일 유효검사");
+		boolean a = findByEmail(email).isPresent();
+		System.out.println("a = " + a);
+		if(a) {
+			throw new DuplicatedEmailException(ErrorMessage.EMAIL_DUPLICATE.getMessage());
+		}
+		System.out.println("회원 객체 생성");
+		Member member = new Member(sequence++, name, birthday, email, gender);
 
-		Member member = new Member(sequence++, name);
+		System.out.println("회원 저장");
 		memberRepository.save(member);
 		return member.getId();
 	}
@@ -25,4 +37,6 @@ public class MemberServiceImpl implements MemberService {
 	public List<Member> findAllMembers() {
 		return memberRepository.findAll();
 	}
+
+	public Optional<Member> findByEmail(String email) { return memberRepository.findByEmail(email); }
 }
