1dacc96a531b6200f33f78b7aee77266b0731c67
seahee <lollol0617@gmail.com>
2025-10-17 00:01:45 +0900

    [#1] feat(NotFoundException): 예외 추가, 수정

--- src/main/java/org/sopt/exception/MyException.java
+++ src/main/java/org/sopt/exception/MyException.java
@@ -1,9 +1,7 @@
 package org.sopt.exception;
 
 public class MyException extends RuntimeException {
-	private final String message;
-
 	public MyException(String message) {
-		this.message = message;
+		super(message);
 	}
 }
